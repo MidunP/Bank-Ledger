b5bcd67218d3a894f5d13ba89b4ed1b42ac421b2
MidunP <midbxy70@gmail.com>
2026-03-18 16:45:00 +0530

    update

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,21 @@
 const mongoose = require("mongoose")
 
-function connectToDB(){
-    mongoose.connect(process.env.MONGO_URI)
-    .then(()=>{
-        console.log("Server Is Connected To DB")
-    })
-    .catch(err => {
-        console.log("Error connecting to DB")
-        process.exit(1)
-    })
+function connectToDB() {
+    return mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+            console.log("Server Is Connected To DB")
+        })
+        .catch(err => {
+            console.error("Error connecting to DB:", err.message || err)
+            process.exit(1)
+        })
 }
+
+// Graceful shutdown handling
+process.on("SIGINT", async () => {
+    await mongoose.connection.close()
+    console.log("MongoDB connection closed through app termination")
+    process.exit(0)
+})
 
 module.exports = connectToDB
